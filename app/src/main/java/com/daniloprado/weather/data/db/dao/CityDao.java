ff6d221dc6c580b2858d6a8e8759fb3b091b7456
DS <coolest.talki.90@gmail.com>
2018-11-01 20:38:00 +0530

    adding new app

--- app/src/main/java/com/daniloprado/weather/data/db/dao/CityDao.java
+++
@@ -1,7 +0,0 @@
-package com.daniloprado.weather.data.db.dao;
-
-import com.daniloprado.weather.model.City;
-import com.j256.ormlite.dao.Dao;
-
-public interface CityDao extends Dao<City, Integer> {
-}
